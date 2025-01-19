845264bb24f9730f0e870fa2e0146c4ef8cd6d7c
Tods <algy25ng@gmail.com>
2025-01-19 20:53:23 +0800

    unit testing untuk isPreload reducer

---
+++ src/states/isPreload/test/reducer.test.js
@@ -0,0 +1,33 @@
+import { describe, expect, it } from 'vitest';
+import isPreloadReducer from '../reducer';
+
+describe('isPreloadReducer function', () => {
+  it('should return the initial state when given by unknown action', () => {
+    // arrange
+    const initialState = [];
+    const action = { type: 'UNKNOWN' };
+    // action
+    const nextState = isPreloadReducer(initialState, action);
+    // assert
+    expect(nextState).toEqual(initialState);
+  });
+
+  it('should set the isPreload when given by SET_IS_PRELOAD action', () => {
+    // arrange
+    const initialState = true;
+    const action = {
+      type: 'isPreload/set',
+      payload: {
+        isPreload: false,
+      },
+    };
+
+    // action
+    const nextState = isPreloadReducer(initialState, action);
+
+    // assert
+    expect(nextState).toEqual(action.payload.isPreload);
+  });
+
+
+});
